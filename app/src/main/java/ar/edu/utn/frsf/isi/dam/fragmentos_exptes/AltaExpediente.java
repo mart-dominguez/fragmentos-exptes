ff5db99d7db2f68555c11857a43f4379d4184daa
mdomingu <mdomingu@gmailcom>
2018-10-23 00:03:15 -0300

    test

--- app/src/main/java/ar/edu/utn/frsf/isi/dam/fragmentos_exptes/AltaExpediente.java
+++ app/src/main/java/ar/edu/utn/frsf/isi/dam/fragmentos_exptes/AltaExpediente.java
@@ -1,11 +1,17 @@
 package ar.edu.utn.frsf.isi.dam.fragmentos_exptes;
 
 
+import android.app.Activity;
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+
+import ar.edu.utn.frsf.isi.dam.fragmentos_exptes.modelo.Expediente;
 
 
 /**
@@ -13,17 +19,47 @@ import android.view.ViewGroup;
  */
 public class AltaExpediente extends Fragment {
 
-
+    public interface OnGuardarExpedienteListener{
+        public void guardar(Expediente e);
+    }
+    private Button btnGuardar;
+    private EditText iniciador;
+    private EditText nro;
+    private EditText tema;
+    private OnGuardarExpedienteListener listenerGuardar;
     public AltaExpediente() {
         // Required empty public constructor
     }
-
+    @Override
+    public void onAttach(Context activity) {
+        super.onAttach(activity);
+        try {
+            listenerGuardar = (OnGuardarExpedienteListener) activity;
+        } catch (ClassCastException e) {
+            throw new ClassCastException(activity.toString() + " must implement OnArticleSelectedListener");
+        }
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_alta_expediente, container, false);
+        View v =inflater.inflate(R.layout.fragment_alta_expediente, container, false);
+        btnGuardar = (Button) v.findViewById(R.id.btnGuardar);
+        iniciador = (EditText) v.findViewById(R.id.expteIniciador);
+        nro = (EditText) v.findViewById(R.id.expteNro);
+        tema = (EditText) v.findViewById(R.id.expteTema);
+        btnGuardar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Expediente e = new Expediente();
+                e.setTema(tema.getText().toString());
+                e.setNro(nro.getText().toString());
+                e.setIniciador(iniciador.getText().toString());
+                listenerGuardar.guardar(e);
+            }
+        });
+        return v;
     }
 
 }
--- app/src/main/java/ar/edu/utn/frsf/isi/dam/fragmentos_exptes/MainActivity.java
+++ app/src/main/java/ar/edu/utn/frsf/isi/dam/fragmentos_exptes/MainActivity.java
@@ -6,16 +6,52 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+import ar.edu.utn.frsf.isi.dam.fragmentos_exptes.dao.ExpedienteRepository;
+import ar.edu.utn.frsf.isi.dam.fragmentos_exptes.modelo.Expediente;
 
+public class MainActivity extends AppCompatActivity implements  AltaExpediente.OnGuardarExpedienteListener{
+    private Button btnAltaExpte;
+    private Button btnAltaPase;
+
+    @Override
+    public void guardar(final Expediente e) {
+        Runnable r = new Runnable() {
+            @Override
+            public void run() {
+                ExpedienteRepository.getInstance(MainActivity.this).getExpteDao().crear(e);
+            }
+        };
+        Thread t = new Thread(r);
+        t.start();
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        MenuFragment mnuFrg = new MenuFragment();
-        FragmentManager frgMngr =  getSupportFragmentManager();
-        frgMngr.beginTransaction()
-                .add(R.id.fragmentContainer,mnuFrg);
+        btnAltaExpte = (Button) findViewById(R.id.btnAltaExpediente);
+        btnAltaExpte.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                AltaExpediente frgAltaExpte = new AltaExpediente();
+                FragmentManager frgMngr =  getSupportFragmentManager();
+                frgMngr.beginTransaction()
+                        .replace(R.id.contenedorFragmento,frgAltaExpte)
+                        .addToBackStack(null)
+                        .commit();
+            }
+        });
+        btnAltaPase = (Button) findViewById(R.id.btnNuevoPase);
+        btnAltaPase.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                AltaPaseFragment paseFragment = new AltaPaseFragment();
+                FragmentManager frgMngr =  getSupportFragmentManager();
+                frgMngr.beginTransaction()
+                        .replace(R.id.contenedorFragmento,paseFragment)
+                        .addToBackStack(null)
+                        .commit();
+            }
+        });
     }
 }
